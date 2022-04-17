d6dde6b589b28da562aff5e523b05b83f5a89bd0
Tyler Steele <teej.steele@gmail.com>
2022-04-17 00:07:47 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+/*!
+* Start Bootstrap - Clean Blog v6.0.8 (https://startbootstrap.com/theme/clean-blog)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
+*/
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function() {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if ( currentTop < scrollPos) {
+            // Scrolling Up
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                console.log(123);
+                mainNav.classList.remove('is-visible', 'is-fixed');
+            }
+        } else {
+            // Scrolling Down
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
+    });
+
+//----------------------------- MY CODE HERE ---------------------------
+
+$('.masthead').ripples({
+	resolution: 512,
+	dropRadius: 20,
+	perturbance: 0.03,
+});
+$(document).ready(function() {
+setInterval(function() {
+		var $el = $('.masthead');
+		var x = Math.random() * $el.outerWidth();
+		var y = Math.random() * $el.outerHeight();
+		var dropRadius = 20;
+		var strength = 0.04 + Math.random() * 0.4;
+
+		$el.ripples('drop', x, y, dropRadius, strength);
+	}, 1800);
+});
+
+
+})
